789fc77707e48cdc3050043ee0776c89831fac92
Roman Korenchuk <Roman Korenchuk>
2017-03-16 16:28:24 +0200

    active device

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 $(function() {
-	$('.desktop-device').on('click', function() {
+	$('#desktop-device').on('click', function() {
 		$('.site-preview').width(1200);
+		$('[id$="-device"]').removeClass('active');
+		$(this).addClass('active');
 	})
 
-	$('.tablet-device').on('click', function() {
+	$('#tablet-device').on('click', function() {
 		$('.site-preview').width(768);
+		$('[id$="-device"]').removeClass('active');
+		$(this).addClass('active');
 	})
 
-	$('.mobile-device').on('click', function() {
+	$('#mobile-device').on('click', function() {
 		$('.site-preview').width(320);
+		$('[id$="-device"]').removeClass('active');
+		$(this).addClass('active');
 	})
 })
